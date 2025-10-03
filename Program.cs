fcb25a00a43e91062fb4a180f1b2251da5c27ce7
Vladislav <studystudy093@gmail.com>
2025-10-03 17:12:23 +0300

    Додатковий коміт для перевірки тригера

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace MyTeamCityProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a sample project for TeamCity CI/CD pipeline.");
         }
     }
 }
--- obj/Debug/net10.0/MyTeamCityProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyTeamCityProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTeamCityProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c3cfab21f495e08019b75f466894cb7ae5889a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTeamCityProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTeamCityProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
